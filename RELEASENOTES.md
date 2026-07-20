0978fdd658bd6902780a5ebda5dea09827828086
Nat! <nat@mulle-kybernetik.com>
2026-07-20 16:59:55 +0200

    CI settings, extension freshening, and prerelease setup

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,3 +1,8 @@
 ## 0.1.0
 
 Various small improvements
+
+
+## 0.1.2
+
+Various small improvements
---
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -0,0 +1,18 @@
+#
+# mulle-dtostr nodetype: ${MULLE__DTOSTR_NODETYPE:-tar}
+#
+export MULLE__DTOSTR_NODETYPE="git"
+
+
+#
+# mulle-dtostr url: ${MULLE__DTOSTR_URL:-https://github.com/mulle-kybernetik-tv/mulle-dtostr/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__DTOSTR_URL="https://github.com/mulle-kybernetik-tv/mulle-dtostr.git"
+
+
+#
+# mulle-dtostr branch: ${MULLE__DTOSTR_BRANCH}
+#
+export MULLE__DTOSTR_BRANCH="prerelease"
+
+
